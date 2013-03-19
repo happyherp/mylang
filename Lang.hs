fa180a99f6ed82e8eeb73b1c7df79dc96322333f
cfreund <carlosfreund@googlemail.com>
2013-03-19 18:46:20 +0100

    addedhandy functions for executing code

--- Lang.hs
+++ Lang.hs
@@ -5,9 +5,18 @@ import Parser
 import Syntax
 import qualified Runtime 
 
-eval :: String -> String
-eval s = case norest parseExpr s of 
-           [(expr,"")] -> show (fst (Runtime.eval expr newState))
+{- High level commands for executing code -}
+
+parseIt :: Parser a -> String -> a
+parseIt parser s= case norest parser s of 
+           [(result,"")] -> result
            [] -> error ("Could not parse "++s)
            exps  -> error ("Multiple interpretations: "++ show exps)
+
+
+eval :: String -> String
+eval s = show (fst (Runtime.eval (parseIt parseExpr s) newState))
+
+run :: String -> String
+run s = show (Runtime.exec (parseIt parseStmt s) newState)
 
